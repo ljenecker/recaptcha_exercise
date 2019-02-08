403b8b10b0babb0b6a75e646b4956a31c461f9ee
ljenecker <ljenecker@gmail.com>
2019-02-08 23:31:22 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+
+app.post('/subscribe', (req, res) => {
+    if (
+        req.body.captcha === undefined ||
+        req.body.captcha === '' ||
+        req.bodycaptcha === null
+    ) {
+        return res.json({
+                success: false,
+                "msg": "Please tick Captcha"
+                })
+    }
+
+    const secretKey = '6LehN5AUAAAAAB08luFK-zPKUdINRzz3UqGZWY-o';
+
+    const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=&${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
+
+    request(verifyUrl, (err, response, body) => {
+            body = JSON.parse(body);
+
+            if (body.sucess !== undefined && !body.success){
+
+                return res.json({
+                    success: false,
+                    "msg": "Failed Captcha verification"
+                    })
+            }
+
+            return res.json({
+                success: true,
+                "msg": "Captcha verification passed"
+                })
+
+            }
+        
+        ); 
+
+})
